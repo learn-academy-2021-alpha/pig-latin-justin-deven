4f537f52e0e458908f880696be1e621fbd249fec
justinhilario <justinhilario@gmail.com>
2021-02-23 15:25:19 -0800

    slice

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     this.state = {
       // "phrase" is the text entered by the user - right now there are some test words hard coded to make the process of testing your code a bit faster and easier
       // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "alpha through yummy squeal queen fry ",
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
       phraseTranslated: "This is where your translated sentence will appear."
     }
@@ -19,17 +19,31 @@ class App extends Component {
   myPigLatinCodeHere = () => {
     // the variable "userInput" will contain the text input from the user modified into an array of words
     // no need to change this variable
-    let userInput = this.state.phrase.split(" ")
+    let userInput = this.state.phrase.split(" ") 
     console.log("userInput:", userInput)
 
-    // now that we have an array of words, we can map over the array and access each word
-    let translatedWordsArray = userInput.map(currentWord => {
-      if (currentWord.findIndexOf(vowelsArray) === 0){
-        return `${currentWord}way`
-      }else if(currentWord.findIndexOf(vowelsArray)!== 0){
-        //if the first index is not a vowel then search the rest of the word for the first vowel
-        return currentWord.search(/[a,e,i,o,u]/i)
-      }
+    // let weirdo = "qu"
+// we are trying to find the first index of occurance for a vowel.
+  
+     let translatedWordsArray = userInput.map(currentWord => {
+
+      let vowels = ['a','e','i','o','u']
+      let array = currentWord.split("")
+      let grade = ['q','u']
+      let word = ""
+
+
+      // if (currentWord.indexOf(vowels) || currentWord.indexOf(vowels) === 0 || currentWord.indexOf(vowels)!== 0) {
+      if (vowels.includes(array[0])) { // finding word that starts with vowel and adding way
+        return `${currentWord}way` 
+      } else if (grade.includes(array[1,2])) {
+        return currentWord.slice(2) + 'ay'
+        // trying to find word that begins with consonant and then the index of first vowel
+      // } else if (currentWord.charAt(0) === "q" && currentWord.charAt(1) === "U") {
+      //   return currentWord.indexOf(vowels, 1) // trying to find first vowel occurance after U in QU
+       }        
+
+// if word index0 = q && index1 = u find .search index of next vowel 
 
       // ACTION ITEM: use "currentWord" as a starting point for your code
       console.log("currentWord:", currentWord)
@@ -38,6 +52,8 @@ class App extends Component {
         return vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o" || vowel === "u"
       })
       console.log("vowelsArray:", vowelsArray)
+
+    
 
       // your code here!!
 
